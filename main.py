6dcf1d57bfcfaceead9384e61223d908b04d8622
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2025-05-06 11:10:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-# Your name
-# Current date
+# Katelyn Curtiss
+# April 6 2025
 # Three Ways to Modify BankAccount Class Attributes
+
+class BankAccount:
+    def __init__(self, account_holder, balence):
+        self.account_holder = account_holder
+        self.balence = balence
+        
+
+    def BankAccount(self):
+        return f"{self.account_holder}{self.balence}"
+    
+my_account = BankAccount('Account Holder: Katelyn Curtiss, ', 'Balence: $1500')
+print(my_account.BankAccount())
+
+my_account = BankAccount('After direct assignment:')
+print(my_account.BankAccount())
+
+
+   
+
+
 
